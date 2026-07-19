d0eaefec58bc374dc482f9af909f71d180a73491
anuragi <coda-ragi@gmail.com>
2026-07-19 09:31:50 +0530

    babel configuration

--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { render, Text } from 'ink';
 
-const Counter = () => {
-  const [counter, setCounter] = useState(0);
+const Example = () => (
+  <>
+    <Text color="green">I am green</Text>
+    <Text color="black" backgroundColor="white">
+      I am black on white
+    </Text>
+    <Text color="#ffffff">I am white</Text>
+    <Text bold>I am bold</Text>
+    <Text italic>I am italic</Text>
+    <Text underline>I am underline</Text>
+    <Text strikethrough>I am strikethrough</Text>
+    <Text inverse>I am inversed</Text>
+  </>
+);
 
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setCounter(previousCounter => previousCounter + 1);
-    }, 100);
-
-    return () => {
-      clearInterval(timer);
-    };
-  }, []);
-
-  return React.createElement(Text, { color: 'green' }, `${counter} tests passed`);
-};
-
-render(React.createElement(Counter));
+render(<Example />);
